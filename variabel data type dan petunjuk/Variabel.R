f74562b65fdf2495b32a4288f80959aa510d7072
fahridika <96968769+fahridika@users.noreply.github.com>
2022-04-20 16:55:45 +0700

    Add files via upload

--- variabel data type dan petunjuk/Variabel.R
+++ variabel data type dan petunjuk/Variabel.R
@@ -3,3 +3,5 @@ names<- "Fahri"
 # untuk variable numeric tidak perlu menggunakan tanda petik
 # <- digunakan untuk mendeklarasikan suatu nilai dalam suatu variabel
 num <- 12
+#kalian dapat menggunakan multiple variabel dengan 1 data.
+text1 <- text2 <-text3<- "Dika"
