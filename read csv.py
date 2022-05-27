b9c73c759ec8fcc3437f047c62748c8068f98cac
MUHAMMAD FAHRI DIKA YUDA <96968769+fahridika@users.noreply.github.com>
2022-05-27 20:15:11 +0700

    Add files via upload

---
+++ read csv.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+
+
+
+# In[21]:
+
+
+ap = pd.read_csv('C:\\Users\\ASUS\\Downloads\\Wong.csv')
+
+
+# In[22]:
+
+
+ap.head()
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+ab = pd.read_csv('D:\\Data Science\\Internship Rakamin Academy\\application_record.csv')
+
+
+# In[6]:
+
+
+ab.head()
+
+
+# In[5]:
+
+
+import pandas as pd
+ac = pd.read_csv('D:\\Data Science\\Internship Rakamin Academy\\credit_record (1).csv')
+ac.head()
+
+
+# In[ ]:
+
+
+
+
