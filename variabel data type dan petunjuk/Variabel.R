de8fbe8d038fed4adeb291c5570d78664613a1c2
MUHAMMAD FAHRI DIKA YUDA <96968769+fahridika@users.noreply.github.com>
2022-06-04 06:16:58 +0700

    Delete variabel data type dan petunjuk directory

--- variabel data type dan petunjuk/Variabel.R
+++
@@ -1,7 +0,0 @@
-#Variable untuk tipe string dan Karakter menggunakan tanda petik.
-names<- "Fahri"
-# untuk variable numeric tidak perlu menggunakan tanda petik
-# <- digunakan untuk mendeklarasikan suatu nilai dalam suatu variabel
-num <- 12
-#kalian dapat menggunakan multiple variabel dengan 1 data.
-text1 <- text2 <-text3<- "Dika"
